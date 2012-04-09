1b9ea8544d269f2f30195ccb2c6e9abb2ffcf4c1
Cedric <cedric@yterium.com>
2012-04-09 12:52:03 +0200

    Gestion des URLS OpenWeb en simple personalisation de arbo+propres du core

---
+++ local_fr.php
@@ -0,0 +1,12 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_core_/plugins/medias/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'titre_type_openweb' => 'URLs OpenWeb',
+);
+
+?>
+
+
