0b568f97a3e27ab0ef542cb96f54b830549e1978
Stephane Deschamps <stephane.deschamps@gmail.com>
2012-04-30 14:49:58 +0200

    i18n 404 et actualites

--- lang/local_en.php
+++ lang/local_en.php
@@ -2,7 +2,10 @@
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	'forbidden_access' => 'Forbidden access'
+	'actualites' => 'News',
+	'forbidden_access' => 'Forbidden access',
+	'le_date' => 'on',
+	'page_not_found' => 'Page not found'
 
 ); // end array
 
--- lang/local_fr.php
+++ lang/local_fr.php
@@ -2,7 +2,10 @@
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	'forbidden_access' => 'Acc&egrave;s interdit'
+	'actualites' => 'Actualit&eacute;s',
+	'forbidden_access' => 'Acc&egrave;s interdit',
+	'le_date' => 'le',
+	'page_not_found' => 'Page non trouv&eacute;e'
 
 ); // end array
 
