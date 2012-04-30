f5fb44c5ed6b2dfee56cf3c9baa0302cbc7aeb9c
Stephane Deschamps <stephane.deschamps@gmail.com>
2012-04-30 15:00:59 +0200

    i18n articles.html

--- lang/local_en.php
+++ lang/local_en.php
@@ -4,6 +4,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	'abstract' => 'Abstract',
 	'actualites' => 'News',
+	'articles' => 'Articles',
+	'articles_all' => 'All the articles on OpenWeb',
+	'articles_none' => 'No article has been published so far.',
 	'auteur' => 'the author',
 	'forbidden_access' => 'Forbidden access',
 	'le_date' => 'on',
--- lang/local_fr.php
+++ lang/local_fr.php
@@ -4,6 +4,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	'abstract' => 'En bref',
 	'actualites' => 'Actualit&eacute;s',
+	'articles' => 'Articles',
+	'articles_all' => 'Tous les articles d\'OpenWeb',
+	'articles_none' => 'Aucun article n\'a &eacute;t&eacute; publi&eacute; pour l\'instant.',
 	'auteur' => 'l\'auteur',
 	'forbidden_access' => 'Acc&egrave;s interdit',
 	'le_date' => 'le',
