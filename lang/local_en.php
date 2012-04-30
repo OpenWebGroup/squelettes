3d1fec786dbc756ad76b6aac6cc0ca17c1492658
Stephane Deschamps <stephane.deschamps@gmail.com>
2012-04-30 14:55:37 +0200

    i18n article

--- lang/local_en.php
+++ lang/local_en.php
@@ -2,10 +2,13 @@
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+	'abstract' => 'Abstract',
 	'actualites' => 'News',
+	'auteur' => 'the author',
 	'forbidden_access' => 'Forbidden access',
 	'le_date' => 'on',
-	'page_not_found' => 'Page not found'
+	'page_not_found' => 'Page not found',
+	'question_remark' => 'A question, a remark? Write to'
 
 ); // end array
 
--- lang/local_fr.php
+++ lang/local_fr.php
@@ -2,10 +2,13 @@
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+	'abstract' => 'En bref',
 	'actualites' => 'Actualit&eacute;s',
+	'auteur' => 'l\'auteur',
 	'forbidden_access' => 'Acc&egrave;s interdit',
 	'le_date' => 'le',
-	'page_not_found' => 'Page non trouv&eacute;e'
+	'page_not_found' => 'Page non trouv&eacute;e',
+	'question_remark' => 'Une question, une remarque&nbsp;? Écrivez &agrave;'
 
 ); // end array
 
