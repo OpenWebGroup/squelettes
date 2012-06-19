acfe2f047f7cf9161abce3b1e26ebf1c393d8189
Cedric <cedric@yterium.com>
2012-06-19 23:26:43 +0200

    Internationalisation

---
+++ lang/local_en.php
@@ -0,0 +1,86 @@
+<?php
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// A
+	'a_la_une' => 'Latest articles',
+	'a_propos_de_cet_article' => 'About this article',
+	'abstract' => 'Abstract',
+	'anonyme' => 'Anonymous',
+	'article' => 'Article',
+	'articles_par' => 'Articles by',
+	'auteur' => 'Author:',
+	'auteurs' => 'Authors:',
+//B
+	'back_to_home' => 'back to the home page',
+//C
+	'ce_site_saffiche_mieux' => 'This site is best viewed in a standards-compliant browser,',
+	'collectif_et_ses_membres' => 'The Group and its members',
+	'commentaire' => 'comment',
+	'commentaires' => 'comments',
+	'commentaires_majuscule' => 'Comments',
+	'commenter' => 'Leave a comment',
+	'conditions_de_reutilisation' => 'Conditions of reuse',
+	'contacts' => 'Contacts',
+	'en_reponse_a' => 'In reply to',
+//D
+//E
+	'entrees_dans_le_site' => 'Entry points in the site',
+	'erreur403' => 'Forbidden access (403)',
+	'erreur404' => 'Page not found (404)',
+	'et_aussi' => 'And also...',
+//F
+//G
+//H
+//I
+//J
+//K
+//L
+	'lancer_la_recherche' => 'launch a search',
+	'le_date' => 'on the',
+	'lui_ecrire' => 'write to him/her',
+//M
+	'membres' => 'The members of the OpenWebGroup',
+	'mise_a_jour' => 'Updated on:',
+//N
+	'navigation' => 'Navigation',
+	'no_comment' => 'Comments closed',
+	'notes' => 'Notes',
+//O
+//P
+	'par' => 'by',
+	'plan_du_site' => 'Site map',
+	'politique_de_confidentialite' => 'Privacy policy (in French)',
+	'proposer_vos_contributions' => 'Submit a contribution',
+	'propulse_par' => 'Propelled by',
+	'publie_le' => 'Published on:',
+//Q
+//R
+	'recherche_resultats' => "results",
+	'recherche_titre' => 'Search',
+	'reperes' => 'Directions',
+	'repondre_a' => 'Reply to',
+//S
+	'sa_biographie' => 'His/her biography',
+	'sa_langue_principale' => 'Main writing language:',
+	'site_heberge_par' => 'Site hosted by',
+	'site_heberge_par2' => 'and by ',
+	'sites_amis' => 'Related websites',
+	'son_site_web' => 'His/her website:',
+	'sur' => 'on',
+	'sur_cette_page' => 'on this page',
+	'sur_les_blogs' => '... and on blogs',
+//T
+	'traduction' => 'This article also exists in English:',
+//U
+//V
+	'voici_pourquoi' => 'this is why',
+	'votre_recherche' => 'Your search:'
+//W
+//X
+//Y
+//Z
+
+); // end array
+
+?>
--- lang/local_fr.php
+++ lang/local_fr.php
@@ -2,29 +2,84 @@
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+// A
+	'a_la_une' => 'Articles à la une',
+	'a_propos_de_cet_article' => '&Agrave; propos de cet article',
+	'abstract' => 'Abstract',
 	'anonyme' => 'Anonyme',
+	'article' => 'Article',
 	'articles_par' => 'Articles de',
+	'auteur' => 'Auteur&nbsp;:',
+	'auteurs' => 'Auteurs&nbsp;:',
+//B
 	'back_to_home' => 'retour &agrave; l&apos;accueil',
+//C
+	'ce_site_saffiche_mieux' => 'Ce site s\'affiche mieux dans un navigateur conforme aux standards,',
+	'collectif_et_ses_membres' => 'Le collectif et ses membres',
+	'commentaire' => 'commentaire',
+	'commentaires' => 'commentaires',
+	'commentaires_majuscule' => 'Commentaires',
 	'commenter' => 'Laisser un commentaire',
+	'conditions_de_reutilisation' => 'Conditions de r&eacute;utilisation',
+	'contacts' => 'Contacts',
 	'en_reponse_a' => 'En réponse &agrave;',
+//D
+//E
+	'entrees_dans_le_site' => 'Entr&eacute;es dans le site',
 	'erreur403' => 'Acc&egrave;s interdit (403)',
 	'erreur404' => 'Page non trouv&eacute;e (404)',
 	'et_aussi' => 'Et aussi...',
+//F
+//G
+//H
+//I
+//J
+//K
+//L
 	'lancer_la_recherche' => 'lancer la recherche',
+	'le_date' => 'le',
 	'lui_ecrire' => 'Lui &eacute;crire',
+//M
+	'membres' => 'Les membres d\'OpenWebGroup',
+	'mise_a_jour' => 'Mise &agrave; jour&nbsp;:',
+//N
+	'navigation' => 'Navigation',
 	'no_comment' => 'Commentaires ferm&eacute;s',
+	'notes' => 'Notes',
+//O
+//P
+	'par' => 'par',
 	'plan_du_site' => 'Plan du site',
+	'politique_de_confidentialite' => 'Politique de confidentialit&eacute;',
+	'proposer_vos_contributions' => 'Proposer vos contributions',
+	'propulse_par' => 'Propuls&eacute; par',
+	'publie_le' => 'Publi&eacute; le&nbsp;:',
+//Q
+//R
 	'recherche_resultats' => "r&eacute;sultats",
 	'recherche_titre' => 'Recherche',
+	'reperes' => 'Rep&egrave;res',
 	'repondre_a' => 'R&eacute;pondre &agrave;',
+//S
 	'sa_biographie' => 'Sa biographie',
 	'sa_langue_principale' => 'Langue principale d&apos;&eacute;criture&nbsp;:',
+	'site_heberge_par' => 'Site h&eacute;berg&eacute; par',
+	'site_heberge_par2' => 'et par l\'',
 	'sites_amis' => 'Des sites amis',
 	'son_site_web' => 'Son site web&nbsp;:',
 	'sur' => 'sur',
 	'sur_cette_page' => 'sur cette page',
 	'sur_les_blogs' => '... et sur les blogs',
+//T
+	'traduction' => 'Cet article existe aussi en fran&ccedil;ais&nbsp;:',
+//U
+//V
+	'voici_pourquoi' => 'voici pourquoi',
 	'votre_recherche' => 'Votre recherche&nbsp;:'
+//W
+//X
+//Y
+//Z
 
 ); // end array
 
